61df394c02dfc326bee2ccdfd76f6f2114ccffa4
JJ Graham <thetwoj@gmail.com>
2021-12-10 13:48:43 -0600

    solution for day 7 part 2

--- day07/main.py
+++ day07/main.py
@@ -12,28 +12,35 @@ def map_crabs(crab_positions):
     return crabs
 
 
-def least_required_moves(crabs):
-    moves_from_left = []
-    moves_from_right = []
+def least_required_fuel(crabs, additive_cost_per_move=0):
+    fuel_from_left = []
+    fuel_from_right = []
     total_moves = []
 
-    current_moves_from_left = 0
-    current_crabs_from_left = 0
+    current_fuel_from_left = 0
+    left_crab_fuel_costs = []  # (crab count, fuel cost)
     for index in range(min(crabs), len(crabs)):
-        moves_from_left.append(current_moves_from_left)
-        current_crabs_from_left += crabs[index]
-        current_moves_from_left += current_crabs_from_left
+        fuel_from_left.append(current_fuel_from_left)
+        for crab_data in left_crab_fuel_costs:
+            crab_data["cost"] += additive_cost_per_move
+            current_fuel_from_left += crab_data["count"] * crab_data["cost"]
+        left_crab_fuel_costs.append({"count": crabs[index], "cost": 1})
+        current_fuel_from_left += crabs[index]
 
-    current_moves_from_right = 0
-    current_crabs_from_right = 0
+
+    current_fuel_from_right = 0
+    right_crab_fuel_costs = []
     for index in range(len(crabs) - 1, min(crabs) - 1, -1):
-        moves_from_right.append(current_moves_from_right)
-        current_crabs_from_right += crabs[index]
-        current_moves_from_right += current_crabs_from_right
-    moves_from_right.reverse()
+        fuel_from_right.append(current_fuel_from_right)
+        for crab_data in right_crab_fuel_costs:
+            crab_data["cost"] += additive_cost_per_move
+            current_fuel_from_right += crab_data["count"] * crab_data["cost"]
+        right_crab_fuel_costs.append({"count": crabs[index], "cost": 1})
+        current_fuel_from_right += crabs[index]
+    fuel_from_right.reverse()
 
     for index in range(0, len(crabs)):
-        total_moves.append(moves_from_left[index] + moves_from_right[index])
+        total_moves.append(fuel_from_left[index] + fuel_from_right[index])
 
     least_moves = min(total_moves)
     least_moves_index = total_moves.index(least_moves) + min(crabs)
@@ -44,9 +51,14 @@ def least_required_moves(crabs):
 def main():
     crab_positions = get_input("input")
     crabs = map_crabs(crab_positions)
-    least_moves, least_moves_index = least_required_moves(crabs)
+    least_fuel, least_fuel_index = least_required_fuel(crabs)
     print("Part 1:")
-    print(f"least moves is {least_moves} at position {least_moves_index}")
+    print(f"least fuel is {least_fuel} at position {least_fuel_index}")
+    print()
+
+    least_fuel, least_fuel_index = least_required_fuel(crabs, additive_cost_per_move=1)
+    print("Part 2:")
+    print(f"least fuel is {least_fuel} at position {least_fuel_index}")
     print()
 
 
